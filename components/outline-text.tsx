7f3be6ea9f2d30eabde9327534631bb0c76cb7c6
abhi <abhiifour@gmail.com>
2026-01-04 12:15:32 +0530

    add outline text

---
+++ components/outline-text.tsx
@@ -0,0 +1,10 @@
+export default function OutlineText() {
+  return (
+    <h1
+      className="text-4xl font-bold text-[#FEC127]"
+      style={{ WebkitTextStroke: "1px black" }}
+    >
+      Outline
+    </h1>
+  );
+}
